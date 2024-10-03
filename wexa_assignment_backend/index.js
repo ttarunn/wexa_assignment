e80f241b4474a39baac1021b1374551e34dd39d3
Tarun <ttarunn0709@gmail.com>
2024-10-03 19:33:55 +0530

    add get req

--- wexa_assignment_backend/index.js
+++ wexa_assignment_backend/index.js
@@ -10,15 +10,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true 
 }));
 
-
-// Rate Limit for apis
+// Rate Limit for APIs
 app.use(rateLimit);
 
 // MongoDB connection
@@ -29,12 +27,17 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-// all routes
+// Default route for hostname
+app.get('/', (req, res) => {
+    res.status(200).json({ status: true });
+});
+
+// All routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const friendRoutes = require('./routes/friendRoutes');
 
-// use routes
+// Use routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/friends', friendRoutes);
